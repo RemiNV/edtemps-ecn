bf92920093f354df139fdd21e7d445007bf0750c
Joffrey <joffrey.terrade@eleves.ec-nantes.fr>
2013-10-11 22:38:25 +0200

    Initialisation de la méthode de sauvegarde d'une salle

--- EdTempsECN/src/org/ecn/edtemps/managers/SalleGestion.java
+++ EdTempsECN/src/org/ecn/edtemps/managers/SalleGestion.java
@@ -1,5 +1,22 @@
 package org.ecn.edtemps.managers;
 
+import org.ecn.edtemps.models.Salle;
+
+/**
+ * Classe de gestion des salles
+ * 
+ * @author Joffrey
+ */
 public class SalleGestion {
+
+	public void sauverSalle(Salle salle) {
+
+		if (salle != null) {
+
+			// BddGestion.executeRequest("INSERT INTO salle () VALUES ()");
+
+		}
+
+	}
 
 }
