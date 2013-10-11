7fc6a98b579cbfff77fc28693c4b651dbfeaeeff
Remi NGUYEN VAN <remi.nguyenvan@gmail.com>
2013-10-11 16:12:08 +0200

    Renommage d'un fichier, casse incorrecte

--- EdTempsECN/src/org/ecn/edtemps/servlets/IdentificationServlet.java
+++ EdTempsECN/src/org/ecn/edtemps/servlets/IdentificationServlet.java
@@ -1,7 +1,16 @@
 package org.ecn.edtemps.servlets;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class IdentificationServlet extends HttpServlet {
 
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		resp.getWriter().println("Epic success");
+	}
 }
