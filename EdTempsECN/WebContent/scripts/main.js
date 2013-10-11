9c2224e2f8db2f0262042468ff6c42419427a396
Remi NGUYEN VAN <remi.nguyenvan@gmail.com>
2013-10-11 11:14:22 +0200

    JavaScript de connexion fonctionnel

--- EdTempsECN/WebContent/scripts/main.js
+++ EdTempsECN/WebContent/scripts/main.js
@@ -9,18 +9,55 @@ require(["lib/davis.min", "RestManager", "jquery"], function(Davis, RestManager)
 	 * jquery est accessible de manière globale par $ (mais il faut tout de même préciser la dépendance
 	 * dans les arguments de require() !), pour ne pas avoir de problème de dépendances (avec jQuery UI notamment) */
 	
-	restManager = new RestManager();
+	var restManager = new RestManager();
 	
-	if(!restManager.isConnected()) {
-		// Chargement de l'interface de connexion
+	var init = function() {
+	
+		if(!restManager.isConnected()) {
+			chargerInterfaceConnection();
+		}
+		else {
+			chargerInterfacePrincipale();
+		}
+	};
+	
+	var chargerInterfaceConnection = function() {
+		// Récupération de l'interface depuis les templates
 		require(["text!../templates/formulaire_connexion.html"], function(htmlFormulaireConnexion) {
-			
+			// Ajout au DOM
 			$(htmlFormulaireConnexion).appendTo($("#main_content").empty());
+			
+			// Callback de connexion
+			$("#btn_connexion").click(function(event) {
+				event.preventDefault();
+				var username = $("#txt_identifiant").val();
+				var pass = $("#txt_password").val();
+				$("#msg_identifiants_invalides").css("display", "none");
+				
+				// Connexion
+				restManager.connexion(username, pass, function(success, identifiantsValides) {
+					if(success) {
+						if(identifiantsValides) {
+							chargerInterfacePrincipale();
+						}
+						else {
+							$("#msg_identifiants_invalides").css("display", "inline");
+						}
+					}
+					else {
+						alert("Erreur de connexion au serveur. Vérifiez votre connexion.");
+					}
+				});
+			});
 		});
-	}
-	else {
-		// TODO : chargement des autres interfaces
-	}
+	};
 	
-	return "test";
+	var chargerInterfacePrincipale = function() {
+		// A implémenter
+		$("#main_content").fadeOut(200, function() {
+			$(this).empty().append("<h1>Connecté. Affichage non implémenté</h1>").fadeIn(200);
+		});
+	};
+	
+	init();
 });
