73b96075ca14a98ab81313add63c12c575345840
Remi NGUYEN VAN <remi.nguyenvan@gmail.com>
2013-10-11 09:25:01 +0200

    Ajout de jquery et correction des inclusions

--- EdTempsECN/WebContent/WEB-INF/scripts/main.js
+++ EdTempsECN/WebContent/WEB-INF/scripts/main.js
@@ -1,5 +1,13 @@
-define([], function() {
-	alert("Script chargé.");
 
-
+/* Fonction d'entrée du programme. 
+ * Le plugin davis est appelé par le mot-clé "davis" (configuré dans index.html) */
+require(["lib/davis.min", "jquery"], function(Davis) {
+	/* Davis est chargé de manière locale avec le mot-clé "Davis" dans cette fonction (passé en argument) : 
+	 * le plugin est configuré pour être chargé de cette manière dans le index.html
+	 * 
+	 * jquery est accessible de manière globale par $ (mais il faut tout de même préciser la dépendance
+	 * dans les arguments de require() !), pour ne pas avoir de problème de dépendances (avec jQuery UI notamment) */
+	
+	
+	return "test";
 });
