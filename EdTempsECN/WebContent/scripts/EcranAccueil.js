68153fb3fafc21665a73c20ab453528d202342f3
Remi NGUYEN VAN <remi.nguyenvan@gmail.com>
2013-10-13 02:10:51 +0200

    Ajout d'evenements de test

--- EdTempsECN/WebContent/scripts/EcranAccueil.js
+++ EdTempsECN/WebContent/scripts/EcranAccueil.js
@@ -6,9 +6,17 @@ define(["lib/fullcalendar.translated.min"], function() {
 	};
 	
 	EcranAccueil.prototype.init = function() {
+	
+		var date = new Date();
+		var d = date.getDate();
+		var m = date.getMonth();
+		var y = date.getFullYear();
+
+	
 		// Initialisation du calendrier
 		$("#calendar").fullCalendar({
 			firstDay: 0,
+			editable: true,
 			defaultView: "agendaWeek",
 			timeFormat: "HH'h'(:mm)",
 			axisFormat: "HH'h'(:mm)",
@@ -27,7 +35,24 @@ define(["lib/fullcalendar.translated.min"], function() {
 				center: 'title',
 				left: 'prev,next today month,agendaWeek,agendaDay'
 			},
-			height: Math.max(window.innerHeight - 150, 500)
+			height: Math.max(window.innerHeight - 150, 500),
+			events: [{
+						title: 'Meeting',
+						start: new Date(y, m, d, 10, 30),
+						allDay: false
+					},
+					{
+						title: 'Repas',
+						start: new Date(y, m, d, 12, 0),
+						end: new Date(y, m, d, 14, 0),
+						allDay: false
+					},
+					{
+						title: 'Cours de GELOL',
+						start: new Date(y, m, d+1, 19, 0),
+						end: new Date(y, m, d+1, 22, 30),
+						allDay: false
+					}]
 		});
 	};
 
