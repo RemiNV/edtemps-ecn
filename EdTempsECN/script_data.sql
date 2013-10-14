444089a39f428701d7a876fc65cf3920e63dd37e
Joffrey <joffrey.terrade@eleves.ec-nantes.fr>
2013-10-15 01:22:24 +0200

    Ajout d'un script d'insertion de données (pour la table matériel)

---
+++ EdTempsECN/script_data.sql
@@ -0,0 +1,4 @@
+
+/* Table des matériels */
+INSERT INTO edt.materiel (materiel_id, materiel_nom) VALUES ('1', 'Ordinateur');
+INSERT INTO edt.materiel (materiel_id, materiel_nom) VALUES ('2', 'Vidéoprojecteur');
