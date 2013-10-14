5bfef59f34c8628254c0fd0c88415085c8a3eb88
Joffrey <joffrey.terrade@eleves.ec-nantes.fr>
2013-10-15 01:19:26 +0200

    Modification des méthodes getSalle() et sauverSalle()

--- EdTempsECN/src/org/ecn/edtemps/managers/SalleGestion.java
+++ EdTempsECN/src/org/ecn/edtemps/managers/SalleGestion.java
@@ -2,12 +2,13 @@ package org.ecn.edtemps.managers;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
+import org.apache.commons.lang3.StringUtils;
 import org.ecn.edtemps.exceptions.DatabaseException;
 import org.ecn.edtemps.exceptions.EdtempsException;
 import org.ecn.edtemps.exceptions.ResultCode;
-import org.ecn.edtemps.models.Materiel;
 import org.ecn.edtemps.models.Salle;
 import org.ecn.edtemps.models.identifie.SalleIdentifie;
 
@@ -17,67 +18,17 @@ import org.ecn.edtemps.models.identifie.SalleIdentifie;
  * @author Joffrey
  */
 public class SalleGestion {
-	
+
 	protected BddGestion _bdd;
-	
+
 	/**
 	 * Initialise un gestionnaire de salles
-	 * @param bdd Gestionnaire de base de données à utiliser
+	 * 
+	 * @param bdd
+	 *            Gestionnaire de base de données à utiliser
 	 */
 	public SalleGestion(BddGestion bdd) {
 		_bdd = bdd;
-	}
-
-	/**
-	 * Enregistrer une salle dans la base de données
-	 * 
-	 * @param salle
-	 *            salle à enregistrer
-	 * 
-	 * @throws EdtempsException
-	 *             en cas d'erreur de connexion avec la base de données
-	 */
-	public void sauverSalle(Salle salle) throws EdtempsException {
-
-		if (salle != null) {
-
-			try {
-
-				// Récupération des arguments sur la salle
-				String batiment = salle.getBatiment();
-				String nom = salle.getNom();
-				Integer niveau = salle.getNiveau();
-				Integer numero = salle.getNumero();
-				Integer capacite = salle.getCapacite();
-				List<Materiel> materiels = salle.getMateriels();
-
-				// Vérification de la cohérence des valeurs
-				if (batiment != null && niveau != null && numero != null
-						&& capacite != null && nom != null) {
-
-					_bdd.executeRequest("INSERT INTO edt.salle (salle_id, salle_batiment, salle_niveau, salle_numero, salle_capacite, salle_nom) VALUES (nextval('edt.seq_salle'), '"
-									+ batiment
-									+ "', '"
-									+ niveau
-									+ "', '"
-									+ numero
-									+ "', '"
-									+ capacite
-									+ "', '"
-									+ nom
-									+ "')");
-
-					// Récupérer l'identifiant de la ligne qui vient d'être
-					// ajoutée
-					_bdd.executeRequest("SELECT currval('edt.seq_salle');");
-				}
-
-			} catch (DatabaseException e) {
-				throw new EdtempsException(ResultCode.DATABASE_ERROR, e);
-			}
-
-		}
-
 	}
 
 	/**
@@ -98,23 +49,37 @@ public class SalleGestion {
 		try {
 
 			// Récupère la salle en base
-			ResultSet resultat = _bdd.executeRequest("SELECT * FROM edt.salle WHERE salle_id="
-							+ identifiant);
+			ResultSet requeteSalle = _bdd
+					.executeRequest("SELECT * FROM edt.salle WHERE salle_id='"
+							+ identifiant + "'");
 
 			// Accède au premier élément du résultat
-			resultat.first();
+			requeteSalle.next();
 
-			if (!resultat.wasNull()) {
+			if (!requeteSalle.wasNull()) {
 				salleRecuperee = new SalleIdentifie();
-				salleRecuperee.setId(resultat.getInt("salle_id"));
-				salleRecuperee
-						.setBatiment(resultat.getString("salle_batiment"));
-				salleRecuperee.setNom(resultat.getString("salle_nom"));
-				salleRecuperee.setNiveau(resultat.getInt("salle_niveau"));
-				salleRecuperee.setNumero(resultat.getInt("salle_numero"));
-				salleRecuperee.setCapacite(resultat.getInt("salle_capacite"));
+				salleRecuperee.setId(requeteSalle.getInt("salle_id"));
+				salleRecuperee.setBatiment(requeteSalle
+						.getString("salle_batiment"));
+				salleRecuperee.setNom(requeteSalle.getString("salle_nom"));
+				salleRecuperee.setNiveau(requeteSalle.getInt("salle_niveau"));
+				salleRecuperee.setNumero(requeteSalle.getInt("salle_numero"));
+				salleRecuperee.setCapacite(requeteSalle
+						.getInt("salle_capacite"));
+				requeteSalle.close();
 
-				// TODO : MATERIEL
+				// Récupérer la liste des matériels de la salle avec la quantité
+				ResultSet requeteMateriel = _bdd
+						.executeRequest("SELECT * FROM edt.contientmateriel WHERE salle_id="
+								+ identifiant);
+				while (requeteMateriel.next()) {
+					salleRecuperee
+							.getMateriels()
+							.put(requeteMateriel.getInt("materiel_id"),
+									requeteMateriel
+											.getInt("contientmateriel_quantite"));
+				}
+				requeteMateriel.close();
 
 			}
 
@@ -125,6 +90,78 @@ public class SalleGestion {
 		}
 
 		return salleRecuperee;
+
+	}
+
+	/**
+	 * Enregistrer une salle dans la base de données
+	 * 
+	 * @param salle
+	 *            salle à enregistrer
+	 * 
+	 * @throws EdtempsException
+	 *             en cas d'erreur de connexion avec la base de données
+	 */
+	public void sauverSalle(Salle salle) throws EdtempsException {
+
+		if (salle != null) {
+
+			try {
+
+				// Récupération des arguments sur la salle
+				String batiment = salle.getBatiment();
+				if (StringUtils.isBlank(batiment)) {
+					batiment = "";
+				}
+				String nom = salle.getNom();
+				Integer niveau = salle.getNiveau();
+				Integer numero = salle.getNumero();
+				Integer capacite = salle.getCapacite();
+				Map<Integer, Integer> materiels = salle.getMateriels();
+
+				// Vérification de la cohérence des valeurs
+				if (StringUtils.isNotBlank(nom)) {
+
+					_bdd.executeRequest("INSERT INTO edt.salle (salle_batiment, salle_niveau, salle_numero, salle_capacite, salle_nom) VALUES ('"
+							+ batiment
+							+ "', '"
+							+ niveau
+							+ "', '"
+							+ numero
+							+ "', '" + capacite + "', '" + nom + "')");
+
+					// Récupérer l'identifiant de la ligne qui vient d'être
+					// ajoutée
+					ResultSet resultat = _bdd
+							.executeRequest("SELECT currval('edt.salle_salle_id_seq');");
+					resultat.next();
+					int lastInsert = resultat.getInt(1);
+
+					// Ajout du lien avec les matériels
+					for (Entry<Integer, Integer> materiel : materiels
+							.entrySet()) {
+						_bdd.executeRequest("INSERT INTO edt.contientmateriel (salle_id, materiel_id, contientmateriel_quantite) VALUES ('"
+								+ lastInsert
+								+ "', '"
+								+ materiel.getKey()
+								+ "', '" + materiel.getValue() + "')");
+					}
+
+				} else {
+					throw new EdtempsException(ResultCode.DATABASE_ERROR,
+							"Tentative d'enregistrer une salle en base de données sans nom.");
+				}
+
+			} catch (DatabaseException e) {
+				throw new EdtempsException(ResultCode.DATABASE_ERROR, e);
+			} catch (SQLException e) {
+				throw new EdtempsException(ResultCode.DATABASE_ERROR, e);
+			}
+
+		} else {
+			throw new EdtempsException(ResultCode.DATABASE_ERROR,
+					"Tentative d'enregistrer un objet NULL en base de données.");
+		}
 
 	}
 }
--- EdTempsECN/test/org/ecn/edtemps/managers/SalleGestionTest.java
+++ EdTempsECN/test/org/ecn/edtemps/managers/SalleGestionTest.java
@@ -1,5 +1,14 @@
 package org.ecn.edtemps.managers;
 
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.Assert;
+
+import org.apache.commons.lang3.StringUtils;
+import org.ecn.edtemps.exceptions.DatabaseException;
 import org.ecn.edtemps.exceptions.EdtempsException;
 import org.ecn.edtemps.models.Salle;
 import org.junit.Before;
@@ -12,19 +21,60 @@ import org.junit.Test;
  */
 public class SalleGestionTest {
 
+	BddGestion bddGestion;
 	SalleGestion salleGestionnaire;
 
 	@Before
-	public void initAllTests() {
-		salleGestionnaire = new SalleGestion();
+	public void initAllTests() throws DatabaseException {
+		bddGestion = new BddGestion();
+		salleGestionnaire = new SalleGestion(bddGestion);
 	}
 
 	@Test
 	public void testSauverSalle() throws EdtempsException {
 
+		// Cas null
 		Salle salle = null;
+		try {
+			this.salleGestionnaire.sauverSalle(salle);
+			fail("Une exception doit être levée");
+		} catch (EdtempsException e) {
+			assertTrue(StringUtils.contains(e.getMessage(), "Tentative d'enregistrer un objet NULL en base de données."));
+		}
+
+		// Cas vide
+		salle = new Salle();
+		try {
+			this.salleGestionnaire.sauverSalle(salle);
+			fail("Une exception doit être levée");
+		} catch (EdtempsException e) {
+			assertTrue(StringUtils.contains(e.getMessage(), "Tentative d'enregistrer une salle en base de données sans nom."));
+		}
+
+		// Cas minimal sans matériel
+		salle.setNom("nom");
+		this.salleGestionnaire.sauverSalle(salle);
+
+		// Cas nominal sans matériel
+		salle.setBatiment("batiment");
+		salle.setCapacite(30);
+		salle.setNiveau(1);
+		salle.setNumero(10);
+		this.salleGestionnaire.sauverSalle(salle);
+
+		// Cas nominal avec matériel
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		map.put(1, 2);
+		map.put(2, 1);
+		salle.setMateriels(map);
 		this.salleGestionnaire.sauverSalle(salle);
 
 	}
 
+	@Test
+	public void testGetSalle() throws EdtempsException {
+		this.salleGestionnaire.getSalle(7);
+	}
+
+	
 }
