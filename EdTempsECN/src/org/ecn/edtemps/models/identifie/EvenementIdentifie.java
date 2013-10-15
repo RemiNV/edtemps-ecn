75edf01bbe5ea5e4b194dc56cf86b7add6f19bc5
Maxime TERRADE <maxime.terrade@eleves.ec-nantes.fr>
2013-10-15 14:15:52 +0200

    Création classes Evenement et EvenementIdentifie

--- EdTempsECN/src/org/ecn/edtemps/models/identifie/EvenementIdentifie.java
+++ EdTempsECN/src/org/ecn/edtemps/models/identifie/EvenementIdentifie.java
@@ -1,7 +1,53 @@
 package org.ecn.edtemps.models.identifie;
 
+import java.util.Date;
+import java.util.List;
+
 import org.ecn.edtemps.models.Evenement;
 
+/**
+ * Classe modèle d'un événement identifié = défini dans la base de données
+ * 
+ * @author Maxime TERRADE
+ */
 public class EvenementIdentifie extends Evenement {
 
+	/** Identifiant de l'evenement dans la base de données */
+	protected int id;
+	
+	
+	/** Constructeur par défaut */
+	public EvenementIdentifie() {
+		super();
+	}
+	
+	/** Constructeur avec tous les attributs */
+	public EvenementIdentifie(String nom, Date dateDebut, Date dateFin,
+			List<Integer> idCalendriers, SalleIdentifie salle,
+			List<String> intervenants, int id) {
+		super(nom, dateDebut, dateFin, idCalendriers, salle, intervenants);
+		this.id=id;
+	}
+	
+	
+	/**
+	 * Getter de l'ID
+	 * 
+	 * @return id
+	 */
+	public int getId() {
+		return id;
+	}
+	
+	/**
+	 * Setter de l'ID
+	 * 
+	 * @param id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
 }
+
+
