a96f3e2b4e696324e80eac7c9ae2b9c8cf856b26
Joffrey <joffrey.terrade@eleves.ec-nantes.fr>
2013-10-18 21:08:00 +0200

    Imports inutiles

--- EdTempsECN/src/org/ecn/edtemps/managers/BddGestion.java
+++ EdTempsECN/src/org/ecn/edtemps/managers/BddGestion.java
@@ -6,7 +6,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.ecn.edtemps.exceptions.DatabaseException;
