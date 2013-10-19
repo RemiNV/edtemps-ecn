16e208df7cd8da03bce0ef9d03903e82ea5a5f90
Joffrey <joffrey.terrade@eleves.ec-nantes.fr>
2013-10-19 11:41:06 +0200

    Organisation du fichier ListGroupesParticipants

--- EdTempsECN/WebContent/scripts/ListeGroupesParticipants.js
+++ EdTempsECN/WebContent/scripts/ListeGroupesParticipants.js
@@ -1,42 +1,41 @@
-define(
-		[ "RestManager" ],
-		function(RestManager) {
+define([ "RestManager" ], function(RestManager) {
 
-			var ListeGroupesParticipants = function(restManager) {
-				this.restManager = restManager;
-			};
+	var ListeGroupesParticipants = function(restManager) {
+		this.restManager = restManager;
+	};
 
-			ListeGroupesParticipants.prototype.getGroupesActifs = function(data) {
+	ListeGroupesParticipants.prototype.getGroupesActifs = function(data) {
 
-				// TODO : ne retourne que les groupes dont les événements sont à
-				// afficher
+		// TODO : ne retourne que les groupes dont les événements sont à
+		// afficher
 
-				return data;
+		return data;
 
-			};
+	};
 
-			/**
-			 * Affiche le bloc "Vos agendas" de l'écran d'accueil avec la liste des groupes de participants
-			 * récupérés en base de donneés
-			 * 
-			 * @param data
-			 */
-			ListeGroupesParticipants.prototype.afficherBlocVosAgendas = function(data) {
+	/**
+	 * Affiche le bloc "Vos agendas" de l'écran d'accueil avec la liste des groupes de participants
+	 * récupérés en base de donneés
+	 * 
+	 * @param data
+	 */
+	ListeGroupesParticipants.prototype.afficherBlocVosAgendas = function(data) {
 
-				// TODO : voir pour faire un tri par ordre alphabétique
-				
-				var str = "";
+		// TODO : voir pour faire un tri par ordre alphabétique
+		
+		var str = "";
 
-				// Génération du code html pour afficher la liste des agendas dans le bloc sur la gauche de l'écran
-				for ( var i = 0; i < data.groupes.length; i++) {
-					var nom = data.groupes[i].nom;
-					str += "<span class='afficher_cacher_groupe'><img src='./img/checkbox_on.png' />"
-							+ nom + "</span><br/>";
-				}
+		// Génération du code html pour afficher la liste des agendas dans le bloc sur la gauche de l'écran
+		for ( var i = 0; i < data.groupes.length; i++) {
+			var nom = data.groupes[i].nom;
+			str += "<span class='afficher_cacher_groupe'><img src='./img/checkbox_on.png' />"
+					+ nom + "</span><br/>";
+		}
 
-				// Affiche le code html généré
-				$("#liste_groupes").html(str);
-			};
+		// Affiche le code html généré
+		$("#liste_groupes").html(str);
+		
+	};
 
-			return ListeGroupesParticipants;
-		});
+	return ListeGroupesParticipants;
+});
