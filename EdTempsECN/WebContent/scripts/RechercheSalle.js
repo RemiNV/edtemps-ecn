0f04a30bf2572431281a1c0d2de4e0c61fbd0919
Joffrey <joffrey.terrade@eleves.ec-nantes.fr>
2013-10-23 09:46:28 +0200

    Timepicker pour les champs de sélection d'heure/minute et avancement de la boite de dialogue de recherche de salle

--- EdTempsECN/WebContent/scripts/RechercheSalle.js
+++ EdTempsECN/WebContent/scripts/RechercheSalle.js
@@ -1,4 +1,4 @@
-define([ "RestManager" ], function(RestManager) {
+define([ "RestManager", "jquerytimeselector" ], function(RestManager) {
 
 	/**
 	 * Constructeur
@@ -6,12 +6,52 @@ define([ "RestManager" ], function(RestManager) {
 	var RechercheSalle = function(restManager) {
 		this.restManager = restManager;
 	};
-	
+
 	RechercheSalle.prototype.afficherFormulaire = function() {
-		$("#form_chercher_salle").html("<p>TOTO</p>");
-		$("#form_chercher_salle").dialog();
+		var me = this;
+		
+		$("#form_chercher_salle").dialog({
+			width: 440,
+			modal: true
+		});
+		 
+		$("#timepickerDebut, #timepickerFin").timepicker({
+			hourText: 'Heures',
+			minuteText: 'Minutes',
+			timeSeparator: 'h',
+			showPeriodLabels: false,
+			showNowButton: false,
+			showCloseButton: false,
+			showDeselectButton: false
+		});
+		
+		$("#form_chercher_salle_valid").click(function() {
+			me.validationFormulaire();
+		});
+		
+	};
+
+	RechercheSalle.prototype.validationFormulaire = function() {
+		var valid = false;
+		
+		
+		
+		if (valid) {
+			this.effectuerRequete();
+		}
 	};
 	
+	RechercheSalle.prototype.effectuerRequete = function() {
+		alert('654');
+	};
+	
+	
+	RechercheSalle.prototype.afficherResultat = function() {
+		
+		$("#form_chercher_salle").fadeOut();
+
+	};
+
 	return RechercheSalle;
 
 });
