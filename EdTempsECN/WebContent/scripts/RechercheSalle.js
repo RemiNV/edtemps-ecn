18b9fd2bc409bbd10527163c90dc2c16e0d2c95b
Joffrey <joffrey.terrade@eleves.ec-nantes.fr>
2013-10-23 10:52:40 +0200

    Dialog de recherche de salle

--- EdTempsECN/WebContent/scripts/RechercheSalle.js
+++ EdTempsECN/WebContent/scripts/RechercheSalle.js
@@ -29,13 +29,37 @@ define([ "RestManager", "jquerytimeselector" ], function(RestManager) {
 			me.validationFormulaire();
 		});
 		
+		$("#form_chercher_salle #dateRechercheSalle, #timepickerDebut, #timepickerFin").click(function() {
+			$(this).css({border: "1px solid gray"});
+		});
+		
 	};
 
 	RechercheSalle.prototype.validationFormulaire = function() {
-		var valid = false;
+		var valid = true;
 		
+		// Validation de la date
+		if ($("#form_chercher_salle #dateRechercheSalle").val()=="") {
+			$("#form_chercher_salle #dateRechercheSalle").css({border: "2px solid red"});
+			$("label[for='dateRechercheSalle']").css({color: "red"});
+			valid = false;
+		}
 		
+		// Validation de l'heure de début
+		if (jQuery.trim($("#form_chercher_salle #timepickerDebut").val())=="") {
+			$("#form_chercher_salle #timepickerDebut").css({border: "2px solid red"});
+			$("label[for='timepickerDebut']").css({color: "red"});
+			valid = false;
+		}
 		
+		// Validation de l'heure de fin
+		if (jQuery.trim($("#form_chercher_salle #timepickerFin").val())=="") {
+			$("#form_chercher_salle #timepickerFin").css({border: "2px solid red"});
+			$("label[for='timepickerFin']").css({color: "red"});
+			valid = false;
+		}
+
+		// Si le formulaire est valide, la requête est effectuée
 		if (valid) {
 			this.effectuerRequete();
 		}
