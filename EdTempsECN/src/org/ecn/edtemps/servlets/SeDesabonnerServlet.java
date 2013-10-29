e22f7d277df1309cf79ce58bbd6d3790b5542b2c
Maxime TERRADE <maxime.terrade@eleves.ec-nantes.fr>
2013-10-29 23:04:00 +0100

    Fin de ligne ?!

--- EdTempsECN/src/org/ecn/edtemps/servlets/SeDesabonnerServlet.java
+++ EdTempsECN/src/org/ecn/edtemps/servlets/SeDesabonnerServlet.java
@@ -1,11 +1,7 @@
 package org.ecn.edtemps.servlets;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonValue;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,12 +9,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.ecn.edtemps.exceptions.DatabaseException;
 import org.ecn.edtemps.exceptions.ResultCode;
-import org.ecn.edtemps.json.JSONUtils;
 import org.ecn.edtemps.json.ResponseManager;
 import org.ecn.edtemps.managers.BddGestion;
 import org.ecn.edtemps.managers.GroupeGestion;
-import org.ecn.edtemps.managers.UtilisateurGestion;
-import org.ecn.edtemps.models.identifie.GroupeIdentifie;
 
 /**
  * Servlet permettant le désabonnement d'un utilisateur à un groupeDeParcipants
@@ -32,15 +25,19 @@ public class SeDesabonnerServlet extends RequiresConnectionServlet {
 	@Override
 	protected void doGetAfterLogin(int userId, BddGestion bdd, HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		
+		logger.debug("Pret à faire le désabonnement");
 		GroupeGestion groupeGestion = new GroupeGestion(bdd);
-		int idGroupe = Integer.parseInt(req.getParameter("idGroupe"));
+		String chaineIdGroupe = req.getParameter("idGroupe");
+		int idGroupe = (int) Integer.parseInt(chaineIdGroupe);
 		
 		try {
 			// Désabonnement du l'utilisateur au groupe (= modification de la BDD)
 			groupeGestion.seDesabonner(userId, idGroupe, true);
-			
+
 			// Génération réponse
 			resp.getWriter().write(ResponseManager.generateResponse(ResultCode.SUCCESS, "Désabonnement à un groupe réussi", null));
+			logger.debug("Désabonnement à un groupe réussi");
+			
 		} catch (DatabaseException e) {
 			resp.getWriter().write(ResponseManager.generateResponse(e.getResultCode(), e.getMessage(), null));
 			logger.error("Erreur d'accès à la base de données lors de la suppression du rattachement", e);
