924428790ba10d7579484edc007d9bfba11207b3
Remi NGUYEN VAN <remi.nguyenvan@gmail.com>
2013-10-30 18:04:30 +0100

    configuration de mockJax pour simuler requete de connexion

--- EdTempsECN/WebContent/scripts/test/mockRestManager.js
+++ EdTempsECN/WebContent/scripts/test/mockRestManager.js
@@ -1,9 +1,33 @@
 define(["mockjax"], function() {
 	
+	var connectionToken = "tokenUnitTestMockjax";
+	var resultCode_Success = 0;
+	var resultCode_IdentificationError = 1;
+	
 	return {
 		
 		mock: function() {
-			console.log("jquery : ", $);
+			$.mockjax({
+				url: "identification/connection",
+				responseTime: 500,
+				contentType: "application/json",
+				response: function(settings) {
+					
+					// Seule connexion possible : userID et pass "unitTest"
+					if(settings.data.username == "unitTest" && settings.data.password == "unitTest") {
+						this.responseText = JSON.stringify({ resultCode: resultCode_Success, message: "", 
+							data: {
+								token: connectionToken
+							}
+						});
+					}
+					else {
+						this.responseText = JSON.stringify({ resultCode: resultCode_IdentificationError, message: "", data: null });
+					}
+				}
+			});
+			
+			console.log("*** Utilisation de mockjax. Les requêtes AJAX jQuery sont interceptées pour les tests unitaires ***");
 		}
 	};
 	
