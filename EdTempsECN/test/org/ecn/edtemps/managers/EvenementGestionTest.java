7b2014ae2b1423dfbe77e02ea510f23eefc7f048
Lixfe <felix.leo.lecuyer@gmail.com>
2013-11-17 21:57:15 +0100

    ecriture des comparaisons de responsables, d'intervenants et salles

--- EdTempsECN/test/org/ecn/edtemps/managers/EvenementGestionTest.java
+++ EdTempsECN/test/org/ecn/edtemps/managers/EvenementGestionTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 import org.ecn.edtemps.exceptions.DatabaseException;
 import org.ecn.edtemps.managers.EvenementGestion;
 import org.ecn.edtemps.models.Evenement;
-import org.ecn.edtemps.models.identifie.EvenementIdentifie;
+import org.ecn.edtemps.models.identifie.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -64,10 +64,53 @@ public class EvenementGestionTest {
 			assertTrue(exists);
 		}
 		
-		
+		//Comparaison des salles rattachées à l'évenement
+			List<SalleIdentifie> sallesCalRecup = evenementRecup.getSalles();
+			List<SalleIdentifie> sallesCal = evenement.getSalles();
+				
+				assertEquals(sallesCalRecup.size(), sallesCal.size());
+				
+				for(SalleIdentifie salleRecup : sallesCalRecup) {
+					boolean exists = false;
+					for(SalleIdentifie salle : sallesCal) {
+						if(salle.getId() == salleRecup.getId())
+							exists = true;
+					}
+					
+					assertTrue(exists);
+				}
 	
+				//Comparaison des intervenants rattachés à l'évenement
+				List<UtilisateurIdentifie> intervenantsCalRecup = evenementRecup.getIntervenants();
+				List<UtilisateurIdentifie> intervenantsCal = evenement.getIntervenants();
+					
+					assertEquals(intervenantsCalRecup.size(), intervenantsCal.size());
+					
+					for(UtilisateurIdentifie intervenantRecup : intervenantsCalRecup) {
+						boolean exists = false;
+						for(UtilisateurIdentifie intervenant : intervenantsCal) {
+							if(intervenant.getId() == intervenantRecup.getId())
+								exists = true;
+						}
+						
+						assertTrue(exists);
+					}
 		
-		
+					//Comparaison des responsables rattachés à l'évenement
+					List<UtilisateurIdentifie> responsablesCalRecup = evenementRecup.getResponsables();
+					List<UtilisateurIdentifie> responsablesCal = evenement.getResponsables();
+						
+						assertEquals(responsablesCalRecup.size(), responsablesCal.size());
+						
+						for(UtilisateurIdentifie responsableRecup : responsablesCalRecup) {
+							boolean exists = false;
+							for(UtilisateurIdentifie responsable : responsablesCal) {
+								if(responsable.getId() == responsableRecup.getId())
+									exists = true;
+							}
+							
+							assertTrue(exists);
+						}
 	}
 	
 
