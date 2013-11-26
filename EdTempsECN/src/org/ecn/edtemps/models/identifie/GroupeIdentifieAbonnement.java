e1603076c5b755d23d3c6a41e61d1ab3c4b3540b
Maxime TERRADE <maxime.terrade@eleves.ec-nantes.fr>
2013-11-26 23:23:10 +0100

    envoie des informations de rattachements des calendriers dans la requete abonnementsEtNonAbonnements

--- EdTempsECN/src/org/ecn/edtemps/models/identifie/GroupeIdentifieAbonnement.java
+++ EdTempsECN/src/org/ecn/edtemps/models/identifie/GroupeIdentifieAbonnement.java
@@ -1,9 +1,12 @@
 package org.ecn.edtemps.models.identifie;
 
+import java.util.List;
+
 import javax.json.Json;
 import javax.json.JsonValue;
 
 import org.ecn.edtemps.json.JSONAble;
+import org.ecn.edtemps.json.JSONUtils;
 
 /**
  * Classe modèle d'un groupe de participants identifié
@@ -18,6 +21,9 @@ public class GroupeIdentifieAbonnement extends GroupeIdentifie implements JSONAb
 	/** information "abonnementObligatoire", pour un utilisateur donnée */
 	protected boolean abonnementObligatoire;
 	
+	/** [uniquement dans le cas d'un groupe unique] ID des groupes de participants rattachés au calendrier associé au groupe unique */
+	protected List<Integer> rattachementsDuCalendrier;
+	
 	/**
 	 * Constructeur utilisant uniquement les informations utiles pour  
 	 * la page d'Abonnements/Désabonnements 
@@ -27,10 +33,13 @@ public class GroupeIdentifieAbonnement extends GroupeIdentifie implements JSONAb
 	 * @param idParent
 	 * @param estCalendrierUnique
 	 * @param abonnementObligatoire
+	 * @param rattachementsDuCalendrier
 	 */
-	public GroupeIdentifieAbonnement(int id, String nom, int idParent, boolean estCalendrierUnique, boolean abonnementObligatoire) {
+	public GroupeIdentifieAbonnement(int id, String nom, int idParent, boolean estCalendrierUnique, 
+			boolean abonnementObligatoire, List<Integer> rattachementsDuCalendrier) {
 		super(id, nom, idParent, estCalendrierUnique);
 		this.abonnementObligatoire = abonnementObligatoire;
+		this.rattachementsDuCalendrier = rattachementsDuCalendrier;
 	}
 
 	@Override
@@ -41,6 +50,7 @@ public class GroupeIdentifieAbonnement extends GroupeIdentifie implements JSONAb
 				.add("parentId", parentId)
 				.add("abonnementObligatoire", abonnementObligatoire)
 				.add("estCalendrierUnique", estCalendrierUnique)
+				.add("rattachementsDuCalendrier", JSONUtils.getJsonIntArray(this.rattachementsDuCalendrier))
 				.build();
 	}
 
