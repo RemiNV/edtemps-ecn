db1c2d55acabb0bf2dc27bd7e8420f1d7734425a
Remi NGUYEN VAN <remi.nguyenvan@gmail.com>
2013-12-02 22:08:24 +0100

    Ajout de la contrainte en bdd : groupe calendrier unique non rattachable

--- EdTempsECN/script_bdd.sql
+++ EdTempsECN/script_bdd.sql
@@ -56,6 +56,9 @@ CREATE TABLE edt.GroupeParticipant (
 				groupeParticipant_aParentCours BOOLEAN NOT NULL DEFAULT FALSE,
 				CONSTRAINT groupeparticipant_pas_propre_parent CHECK (groupeparticipant_id <> groupeparticipant_id_parent),
   				CONSTRAINT groupeparticipant_pas_propre_parent_tmp CHECK (groupeparticipant_id <> groupeparticipant_id_parent_tmp),
+  				CONSTRAINT groupeparticipant_calendrierUnique_non_rattachable CHECK (NOT groupeParticipant_estCalendrierUnique OR 
+  					(NOT groupeParticipant_rattachementAutorise AND groupeParticipant_id_parent IS NULL AND groupeParticipant_id_parent_tmp IS NULL)
+  				), -- Les groupes "calendriers unique" ne peuvent pas être rattachés
                 CONSTRAINT groupeparticipant_id PRIMARY KEY (groupeParticipant_id)
 );
 
@@ -96,6 +99,7 @@ CREATE TABLE edt.Evenement (
                 eve_dateDebut TIMESTAMP,
                 eve_dateFin TIMESTAMP,
 				eve_createur INTEGER,
+				CONSTRAINT eve_dates_coherentes CHECK (eve_dateDebut <= eve_dateFin),
                 CONSTRAINT eve_id PRIMARY KEY (eve_id)
 );
 
