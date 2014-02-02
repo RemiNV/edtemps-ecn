04a065eca21c8662d2b0d776afe64549568d0387
Joffrey <joffrey.terrade@eleves.ec-nantes.fr>
2014-02-02 01:18:42 +0100

    Avancement de l'écran des jours bloqués

--- EdTempsECN/WebContent/scripts/planning_cours/CalendrierAnnee.js
+++ EdTempsECN/WebContent/scripts/planning_cours/CalendrierAnnee.js
@@ -8,31 +8,54 @@ define([ "RestManager" ], function(RestManager) {
 	 * @constructor
 	 * @alias CalendrierAnnee
 	 */
-	var CalendrierAnnee = function(restManager, jqCalendar, annee) {
+	var CalendrierAnnee = function(restManager, jqEcran, jqCalendar, events, callback) {
 		this.restManager = restManager;
+		this.jqEcran = jqEcran;
 		this.jqCalendar = jqCalendar;
-		this.annee = annee;
+		this.premierAffichage = true;
+		this.callback = callback;
+		
+		// Récupère l'année scolaire à afficher en fonction de la date du jour
+		var today = new Date();
+		this.annee = today.getFullYear();
+		if (today.getMonth() >= 0 && today.getMonth() <= 7) this.annee = today.getFullYear()-1;
 
 		// Initialise les noms des mois, des jours et les nombres de jours par mois
-	    this.listeMois = new Array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
-	    this.listeJours = new Array('Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi');
-	    this.listeNbJours = new Array(31, nbJoursFevrier(), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
-	    
-	    // Initialise le calendrier et l'affiche dans l'objet jqCalendar
-		this.init();
+	    this.listeMois = new Array('Septembre', 'Octobre', 'Novembre', 'Décembre', 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août');
+	    this.listeMoisCourts = new Array('Sep.', 'Oct.', 'Nov.', 'Déc.', 'Jan.', 'Fév.', 'Mar.', 'Avr.', 'Mai', 'Juin', 'Juil.', 'Août');
+	    this.listeMoisNumero = new Array(9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8);
+	    this.listeNbJours = new Array(30, 31, 30, 31, 31, 28, 31, 30, 31, 30, 31, 31);
 	};
 	
 	
 	/**
-	 * Initialisation du calendrier dans la div fournie en paramètre
+	 * Getter de l'année
 	 */
-	CalendrierAnnee.prototype.init = function() {
-	    var content = "<table id='calendrierAnnee'>";
+	CalendrierAnnee.prototype.getAnnee = function() {
+		return this.annee;
+	};
+	
+	
+	/**
+	 * Initialisation du calendrier
+	 */
+	CalendrierAnnee.prototype.afficherCalendrier = function() {
+		var me = this;
+		
+		// Met à jour le nomrbre de jour du mois de février en fonction de l'année
+		this.listeNbJours[5] = this.nbJoursFevrier();
+		
+		// Prépare le contenu du calendrier à afficher
+		var content = "<table id='calendrierAnnee'>";
 
 	    // Ligne de titre
 	    content += "<tr>";
 		for (var i=0; i<12; i++) {
-			content += "<th>"+this.listeMois[i]+"</th>";
+			if (i<4) {
+				content += "<th title='"+this.listeMois[i]+"'>"+this.listeMoisCourts[i]+"</th>";
+			} else {
+				content += "<th title='"+this.listeMois[i]+"'>"+this.listeMoisCourts[i]+"</th>";
+			}
 		}
 		content += "</tr>";
 
@@ -43,7 +66,9 @@ define([ "RestManager" ], function(RestManager) {
 			content += "<tr>";
 			for (var j=0; j<12; j++) {
 				if (this.listeNbJours[j] >= (i+1)) {
-					content += "<td>"+(i+1)+"</td>";
+					var an = this.annee;
+					if (this.listeMoisNumero[j] >= 1 && this.listeMoisNumero[j] <= 8) { an = this.annee+1; }
+					content += "<td class='jour_bloque_clic' date='"+an+"-"+this.listeMoisNumero[j]+"-"+(i+1)+"'>"+(i+1)+"</td>";
 				} else {
 					content += "<td></td>";
 				}
@@ -54,22 +79,56 @@ define([ "RestManager" ], function(RestManager) {
 
 		content += "</table>";
 		
-		// Affichage du calendrier
-		this.jqCalendar.html(content);
 		
+		
+		// Affichage du calendrier
+		this.jqCalendar.html(content).fadeIn(700);
+		
+		
+		// Affichage de l'année scolaire
+		this.jqEcran.find("#numero_annee_scolaire").html(this.annee + " - " + (this.annee + 1));
+		
+		
+	    // Affecte la fonction de callback sur les jours cliquables
+	    this.jqEcran.find(".jour_bloque_clic").click(function() {
+	    	me.callback($(this));
+	    });
+	    
+	};
+
+	
+	/**
+	 * Charger une année particulière
+	 * @param {int} annee Numéro de l'année
+	 * @param {events} events Liste des événements à afficher
+	 */
+	CalendrierAnnee.prototype.chargerAnnee = function(annee, events) {
+		this.annee = annee;
+		this.events = events; // Les événements à afficher (jours bloqués)
+
+		if (this.premierAffichage) {
+			this.premierAffichage = false;
+			
+			this.afficherCalendrier();
+		} else {
+			var me = this;
+			this.jqCalendar.fadeOut(200, function () {
+				me.afficherCalendrier();
+			});
+		}
 	};
 
 
 	/**
 	 * Récupère le nombre de jours du mois de février en fonction de l'année
 	 */
-	function nbJoursFevrier() {
-		if ((new Date(this.annee, 1, 29, 0, 0, 0, 0)).getDate() == 29) {
-			return 29; 
+	CalendrierAnnee.prototype.nbJoursFevrier = function() {
+		if ((new Date(this.annee + 1, 1, 29, 0, 0, 0, 0)).getDate() == 29) {
+			return 29;
 		} else {
 			return 28;
 		}
-	}
+	};
 
 	
 	return CalendrierAnnee;
--- EdTempsECN/WebContent/scripts/planning_cours/EcranJoursBloques.js
+++ EdTempsECN/WebContent/scripts/planning_cours/EcranJoursBloques.js
@@ -11,8 +11,36 @@ define([ "planning_cours/CalendrierAnnee", "jquery"], function(CalendrierAnnee) 
 	 */
 	var EcranJoursBloques = function(restManager) {
 		this.RestManager = restManager;
+		this.jqEcran = $("#jours_bloques");
+		var me = this;
+
+		// Initialise le calendrier
+		this.calendrierAnnee = new CalendrierAnnee(restManager, this.jqEcran, $("#calendar_jours_boques"), null, function(jour) {
+			alert(jour.attr("date"));
+		});
 		
-		this.calendrierAnnee = new CalendrierAnnee(restManager, $("#calendar_jours_boques"), 2014);
+		var annee = this.calendrierAnnee.getAnnee();
+		this.calendrierAnnee.chargerAnnee(annee, this.recupererJoursBloquesAnnee(annee));
+		
+	    // Affecte les fonctions aux flêches
+	    this.jqEcran.find("#annee_precedente").click(function() {
+	    	var annee = me.calendrierAnnee.getAnnee() - 1;
+	    	me.calendrierAnnee.chargerAnnee(annee, me.recupererJoursBloquesAnnee(annee));
+	    });
+	    this.jqEcran.find("#annee_suivante").click(function() {
+	    	var annee = me.calendrierAnnee.getAnnee() + 1;
+	    	me.calendrierAnnee.chargerAnnee(annee, me.recupererJoursBloquesAnnee(annee));
+	    });
+
+	};
+	
+
+	/**
+	 * Récupère les jours bloqués d'une année
+	 * @param {int} annee Numéro de l'année
+	 */
+	EcranJoursBloques.prototype.recupererJoursBloquesAnnee = function(annee) {
+		
 	};
 	
 	
